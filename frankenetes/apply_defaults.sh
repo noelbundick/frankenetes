b679a7099212d5312b7d1a951486024c80a440a8
Noel Bundick <noelbundick@gmail.com>
2018-07-14 17:54:15 -0700

    Fix more paths

--- frankenetes/apply_defaults.sh
+++ frankenetes/apply_defaults.sh
@@ -1,7 +1,7 @@
 export KUBECONFIG=$1
 
-# Execute in the script directory
-cd `dirname $0`
+# Execute in the defaults directory
+cd `dirname $0`/defaults
 
 kubectl apply -f ClusterRoleBinding.yml
 kubectl apply -f LimitRange.yml
